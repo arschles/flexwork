d537d38b0b36d7ff9cfc757bc5aefa62328e88d9
Aaron Schlesinger <arschles@gmail.com>
2015-11-02 14:59:12 -0800

    json cleanup

--- json/error.go
+++ json/error.go
@@ -1,40 +1,21 @@
 package json
 
-import (
-	"errors"
+import "github.com/arschles/flexwork"
 
-	"github.com/arschles/flexwork"
-)
-
-type Err error
-
-func NewErr(str string) Err {
-	return Err(errors.New(str))
-}
-
-var (
-	ErrFormatting   = NewErr("json_formatting")
-	ErrDB           = NewErr("database")
-	ErrUnauthorized = NewErr("unauthorized")
-	ErrBadRequest   = NewErr("bad request")
-	ErrInternal     = NewErr("internal_error")
-	ErrNotFound     = NewErr("not_found")
-)
-
-const (
-	jsonEncodeErr = `{
-    "error":"json",
-    "msg":"couldn't encode JSON indicating the error"
-  }`
-)
-
+// ErrorJSON is an error to be encoded to JSON and returned. See constants in this package
+// for some commonly used ones
 type ErrorJSON struct {
+	Code    int    `json:"-"`
 	Type    string `json:"error"`
 	Message string `json:"msg"`
 }
 
+const jsonEncodeErr = `{
+  "error":"json",
+  "msg":"couldn't encode JSON indicating the error"
+}`
+
 // Error returns a flexwork.Response that encodes a JSON error response
-func Error(code int, eType Err, msg string) flexwork.Response {
-	err := ErrorJSON{Type: eType.Error(), Message: msg}
-	return Encode(code, err)
+func Error(err ErrorJSON) flexwork.Response {
+	return Encode(err.Code, err)
 }
