f3b83adebd3b3d6fe2595068856e7469ad1e5304
Aaron Schlesinger <arschles@gmail.com>
2015-11-02 16:05:19 -0800

    better docs

--- json/doc.go
+++ json/doc.go
@@ -1,2 +1,7 @@
-// Package json is a convenience package for writing JSON APIs
+// Package json is a convenience package for writing JSON APIs. Sample usage:
+//
+//  http.Handle(flexwork.WrapFunc(func(r *http.Request) Response {
+//    // ...
+//    json.Encode(http.StatusOK, someData)
+//  }))
 package json
