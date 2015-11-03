e895f7a8a6a9da4fe2fe0901a9bf21c78bed3686
Aaron Schlesinger <arschles@gmail.com>
2015-11-02 16:18:55 -0800

    more docs

--- doc.go
+++ doc.go
@@ -8,4 +8,18 @@
 //
 // This change is significant because the framework (or you) can use each returned Response as a signal
 // that the request is finished (or at least part of it has finished). More information to come here.
+//
+// Example usage:
+//
+//  func main() {
+//    mux := http.NewServeMux() // you can use any kind of router here
+//    mux.Handle("/path1", flexwork.WrapFunc(func(r *http.Request) Response {
+//      //...
+//      if r.Method == "GET" {
+//        return flexwork.Code(http.StatusOK)
+//      }
+//      return flexwork.Code(http.StatusNotFound)
+//    }))
+//    log.Fatal(http.ListenAndServe(":8080", mux))
+//  }
 package flexwork
